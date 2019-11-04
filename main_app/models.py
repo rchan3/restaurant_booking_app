9aa443fc64cb5eee1bbd1663792f050a0802fabc
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-11-04 16:28:04 -0500

    login feature implemented both backend/frontend design

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import datetime
 
 # Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	location = models.CharField(max_length=100)
+	first_name = models.CharField(max_length=50) 
+	last_name = models.CharField(max_length=50)
+	email = models.EmailField(max_length=100)
+	phone = models.IntegerField()
+	createdAt = models.DateTimeField(default=datetime.now)
+
+	def __str__(self):
+		return first_name + " " + last_name
