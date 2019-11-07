872ed15e52ddc5dad507ddb6d99c899282371d26
Rachel Chan <r.achiechan@gmail.com>
2019-11-07 16:59:51 -0500

    fix merge issues

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,4 +1,5 @@
-from .models import Profile, Restaurant, Menu
+from .models import Profile, Restaurant, Menu, Reservation
+from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
